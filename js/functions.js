682167c80980a631f4486388368b2a2cf5b4f205
Юлия Перелыгина <julyvol@yahoo.com>
2026-04-08 22:22:21 +0300

    добавляет функции для тренировки

---
+++ js/functions.js
@@ -0,0 +1,53 @@
+const checkLength = (string = '', maxSymbols = 1) => string.length <= maxSymbols;
+
+function comparesStringLength(string, length) {
+  if (string.length <= length) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+
+const isPalindrome = (string = '') => {
+
+  string = string.replaceAll('', '').toLowerCase();
+
+  let reversed = '';
+
+  for (let i = string.length - 1; i >= 0; i--) {
+    reversed += string[i];
+  }
+
+  return string === reversed;
+
+};
+
+const verifyPalindrome = (string) => {
+  const normalizedString = string.replaceAll('', '').toUpperCase();
+  const reverseString = normalizedString.split('').reverse().join('');
+  return reverseString === normalizedString;
+};
+
+
+const extractNumbers = (string) => {
+  let result = '';
+
+  string = string.toString();
+
+  for (let i = 0; i <= string.length - 1; i++) {
+    if (Number.isNaN(parseInt(string[i], 10)) === false) {
+      result += string[i];
+    }
+  }
+
+  return result === '' ? NaN : Number(result);
+};
+
+
+function stringToNumber(str) {
+  return Number(
+    [...str].filter((item) => !isNaN(parseInt(item, 10))).join('') || NaN
+  );
+}
+

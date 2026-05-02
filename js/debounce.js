a13687262a0c7a1bb9c23935d1529c3c53f0facf
Юлия Перелыгина <julyvol@yahoo.com>
2026-05-02 21:01:54 +0300

    добавляет обработчики изменения фильтров

---
+++ js/debounce.js
@@ -0,0 +1,12 @@
+const DEBOUNCE_DELAY = 500;
+
+const debounce = (callback, timeoutDelay = DEBOUNCE_DELAY) => {
+  let timeoutId;
+
+  return (...rest) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+  };
+};
+
+export { debounce };
--- js/main.js
+++ js/main.js
@@ -5,10 +5,12 @@ import { initBigPictureHandlers } from './big-picture.js';
 import { initFormUpload } from './form-upload.js';
 import { getData } from './api.js';
 import { showDataError } from './messages.js';
+import { initFilters } from './filters.js';
 
 getData()
   .then((pictures) => {
     renderPictures(pictures);
+    initFilters(pictures);
   })
   .catch(() => {
     showDataError();
